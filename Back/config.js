a44787e60f225b3bb4e33c3f5c405f4a7c87888a
Dranoq <dranoq@zoho.eu>
2018-10-29 23:56:36 +0200

    Add draft back (without mysql)

---
+++ Back/config.js
@@ -0,0 +1,5 @@
+const config = {}
+
+config.appPort = 3000
+
+module.exports = config
---
+++ Back/index.js
@@ -0,0 +1,73 @@
+const config = require('./config')
+const log = console.log
+
+const express = require('express')
+const fs = require('fs')
+const url = require('url')
+const path = require('path')
+const download = require('download-file')
+const sharp = require('sharp')
+sharp.cache(false)
+
+function detectName (imageUrl) {
+  let parsed = url.parse(imageUrl)
+  return path.basename(parsed.pathname)
+}
+
+function removeFile (fileUrl) {
+  const filePath = fileUrl
+  fs.access(filePath, err => {
+    if (!err) {
+      fs.unlink(filePath, function (err) {
+        if (err) {
+          console.log('rem', err)
+        }
+      })
+    } else {
+      console.log('rem', err)
+    }
+  })
+}
+
+// TODO: make dirs if not exist
+
+const app = express()
+
+app.get('/', (req, res) => {
+  let url = req.query.url // TODO: check url
+  let jpegQuality = Number(req.query.jpegQuality) || 50
+  // let pngCompress = req.query.pngCompress || 9
+  let dirName = './images-temp/'
+  let fileName = detectName(url)
+  let fileTemp = `./images-temp/${fileName}`
+
+  const options = {
+    directory: dirName,
+    filename: fileName
+  }
+
+  download(url, options, function (err) {
+    if (err) { // TODO: throw
+      log('Download error')
+      res.json({ result: false })
+      return
+    }
+
+    // TODO: download file size
+    // TODO: png .png({ compressionLevel: * })
+
+    sharp(fileTemp)
+      .jpeg({ quality: jpegQuality })
+      .toFile(`./images-compress/${fileName}`, function (err) {
+        if (err) {
+          log('Image convert error:', err)
+          res.json({ result: false })
+        } else { // TODO: compress file size
+          removeFile(fileTemp)
+          res.json({ result: true })
+        }
+      })
+  })
+})
+
+app.listen(config.appPort, () => log(`Started on port ${config.appPort}`))
