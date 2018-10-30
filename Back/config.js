44d5d667a505915393cdc4d4e114b089a8fd28ff
Dranoq <dranoq@zoho.eu>
2018-10-30 16:50:49 +0200

    Add json with some image data Move dirs to config, create if not exists Gen hash for compress images

--- Back/config.js
+++ Back/config.js
@@ -2,4 +2,7 @@ const config = {}
 
 config.appPort = 3000
 
+config.dirTemp = './images-temp/'
+config.dirCompress = './images-compress'
+
 module.exports = config
--- Back/index.js
+++ Back/index.js
@@ -4,11 +4,20 @@ const log = console.log
 const express = require('express')
 const cors = require('cors')
 const fs = require('fs')
-const url = require('url')
 const path = require('path')
-const download = require('download-file')
+const sha256 = require('sha256')
 const sharp = require('sharp')
+const url = require('url')
+const download = require('image-downloader')
+
 sharp.cache(false)
+
+if (!fs.existsSync(config.dirTemp)) {
+  fs.mkdirSync(config.dirTemp)
+}
+if (!fs.existsSync(config.dirCompress)) {
+  fs.mkdirSync(config.dirCompress)
+}
 
 function detectName (imageUrl) {
   let parsed = url.parse(imageUrl)
@@ -16,60 +25,78 @@ function detectName (imageUrl) {
 }
 
 function removeFile (fileUrl) {
+  log('[Remove file]', fileUrl)
   const filePath = fileUrl
   fs.access(filePath, err => {
     if (!err) {
       fs.unlink(filePath, function (err) {
         if (err) {
-          console.log('rem', err)
+          console.log('rm', err)
         }
       })
     } else {
-      console.log('rem', err)
+      console.log('rm', err)
     }
   })
 }
 
-// TODO: make dirs if not exist
+function getFileSizeMb (filename) {
+  const stats = fs.statSync(filename)
+  const fileSizeInBytes = stats.size
+  const mb = (fileSizeInBytes / 1024000.0).toFixed(2)
+  return mb
+}
 
 const app = express()
 app.use(cors())
 
-app.get('/', (req, res) => {
-  let url = req.query.url // TODO: check url
-  let jpegQuality = Number(req.query.jpegQuality) || 50
-  // let pngCompress = req.query.pngCompress || 9
-  let dirName = './images-temp/'
-  let fileName = detectName(url)
-  let fileTemp = `./images-temp/${fileName}`
+app.get('/add', (req, res) => {
+  let imageUrl = req.query.url // TODO: check url
+  let jpegQuality = Math.round(req.query.jpegQuality) || 50
+  let pngCompress = Math.round(req.query.pngCompress) || 9
+  let fileName = sha256(Date.now().toString())
+  let compressFile = path.join(config.dirCompress, fileName + '.jpeg')
+  let imageSize = 0
+  let compressSize = 0
 
-  const options = {
-    directory: dirName,
-    filename: fileName
+  const downloadOptions = {
+    url: imageUrl,
+    dest: path.join(config.dirTemp, fileName + '.jpeg')
   }
 
-  download(url, options, function (err) {
-    if (err) { // TODO: throw
-      log('Download error')
-      res.json({ result: false })
-      return
-    }
+  download.image(downloadOptions)
+    .then(({ filename, image }) => {
+      let tempName = filename
 
-    // TODO: download file size
-    // TODO: png .png({ compressionLevel: * })
+      log(`[Saved] ${tempName}`)
+      imageSize = getFileSizeMb(tempName)
 
-    sharp(fileTemp)
-      .jpeg({ quality: jpegQuality })
-      .toFile(`./images-compress/${fileName}`, function (err) {
-        if (err) {
-          log('Image convert error:', err)
-          res.json({ result: false })
-        } else { // TODO: compress file size
-          removeFile(fileTemp)
-          res.json({ result: true })
-        }
-      })
-  })
+      sharp(tempName)
+        .jpeg({ quality: jpegQuality })
+        .toFile(compressFile, err => {
+          if (err) {
+            log('[Convert error]', err)
+            res.json({ result: false, error: 'Convert' })
+          } else {
+            compressSize = getFileSizeMb(compressFile)
+            removeFile(tempName)
+
+            res.json({ result: true,
+              date: Date.now(),
+              image: {
+                original: { name: detectName(imageUrl), size: imageSize },
+                compress: { name: fileName + '.jpeg', size: compressSize, comperss: jpegQuality }
+              }
+            })
+          }
+        })
+    })
+    .catch((err) => {
+      console.error('[Download Error]\n', err)
+      res.json({ result: false, error: 'Download' })
+    })
+
+  // TODO: png .png({ compressionLevel: * })
 })
 
 app.get('/all', (req, res) => {
