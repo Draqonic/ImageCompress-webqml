55ed2e664ab3a4322b36bd00f3a8afe86b1e6675
Dranoq <dranoq@zoho.eu>
2018-10-31 16:33:07 +0200

    Update sql

--- Back/sql/images.sql
+++ Back/sql/images.sql
@@ -4,9 +4,10 @@ DROP TABLE IF EXISTS `images`.`image_list`;
 CREATE TABLE `images`.`image_list`  (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(500) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `original_url` varchar(1000) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   `original_size` double NULL DEFAULT NULL,
   `compress_size` double NULL DEFAULT NULL,
   `file` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   `time` datetime(0) NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP(0),
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 17 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 0 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
